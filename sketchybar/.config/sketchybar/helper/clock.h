08d22ea32d739b0a95e08b49b1894315be9f2f12
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-02-28 17:56:10 +0600

    add am pm

--- sketchybar/.config/sketchybar/helper/clock.h
+++ sketchybar/.config/sketchybar/helper/clock.h
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
-struct clock {
+struct clock
+{
   char command[128];
 };
 
-static inline void clock_init(struct clock* clock) {
+static inline void clock_init(struct clock *clock)
+{
   snprintf(clock->command, 128, "");
 }
-
-static inline void clock_update(struct clock* clock) {
+// https://www.ibm.com/docs/en/i/7.1?topic=functions-strftime-convert-datetime-string
+static inline void clock_update(struct clock *clock)
+{
   time_t t = time(NULL);
-  struct tm* tm = localtime(&t);
+  struct tm *tm = localtime(&t);
 
-  char time[8];
-  strftime(time, 8, "%H:%M", tm);
+  char time[128];
+  strftime(time, 128, "%I:%M %p", tm);
 
   char date[16];
   strftime(date, 16, "%a %d. %b", tm);

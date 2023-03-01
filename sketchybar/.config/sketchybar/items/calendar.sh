b2e4dbbb831893b385360faa9fede65a7e52ce5a
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-03-01 06:45:35 +0600

    brighter datetime

--- sketchybar/.config/sketchybar/items/calendar.sh
+++ sketchybar/.config/sketchybar/items/calendar.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
-
+# icon.color=$LABEL_COLOR \
+# label.color=$LABEL_COLOR \
 sketchybar --add item calendar right \
     --set calendar icon=cal \
-    icon.color=$LABEL_COLOR \
     icon.font="$FONT:Bold:14.0" \
     icon.padding_left=5 \
     icon.padding_right=5 \
-    label.color=$LABEL_COLOR \
     label.padding_left=5 \
     label.padding_right=5 \
     width=180 \

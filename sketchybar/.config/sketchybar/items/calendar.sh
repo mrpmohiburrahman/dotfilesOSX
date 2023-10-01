81af5ec855bef2ba0cd901ff682c05e97dd9b0d5
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-10-01 21:25:49 +0600

    12 hour format

--- sketchybar/.config/sketchybar/items/calendar.sh
+++ sketchybar/.config/sketchybar/items/calendar.sh
@@ -4,7 +4,7 @@ calendar=(
   icon=cal
   icon.font="$FONT:Black:12.0"
   icon.padding_right=0
-  label.width=45
+  label.width=75
   label.align=right
   padding_left=15
   update_freq=30
@@ -12,6 +12,6 @@ calendar=(
   click_script="$PLUGIN_DIR/zen.sh"
 )
 
-sketchybar --add item calendar right       \
-           --set calendar "${calendar[@]}" \
-           --subscribe calendar system_woke
+sketchybar --add item calendar right \
+  --set calendar "${calendar[@]}" \
+  --subscribe calendar system_woke

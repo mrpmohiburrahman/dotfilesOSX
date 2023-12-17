2e2df30712d14081437f3760a0e8ee56273b1317
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-12-17 11:38:34 +0600

    using /bin/bash to install home brew

--- homebrewSetup/brewInstall.sh
+++ homebrewSetup/brewInstall.sh
@@ -8,7 +8,8 @@ sudo -v
 # Check for Homebrew and install it if missing
 if test ! $(which brew); then
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 

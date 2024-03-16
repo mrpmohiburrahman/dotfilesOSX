6a255afe010477560a9681497cdc86151b01b532
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-03-16 19:50:01 +0600

    cycle throught available windows

--- yabai/switch_next_window.sh
+++ yabai/switch_next_window.sh
@@ -1,15 +1,27 @@
-#!/bin/bash
+#!/bin/dash
 
 # Get the ID of the currently focused window
-current_window=$(yabai -m query --windows --window | jq '.id')
+current_window=$(/opt/homebrew/bin/yabai -m query --windows --window | /opt/homebrew/bin/jq '.id')
 
 # Get the app of the currently focused window
-current_app=$(yabai -m query --windows --window | jq -r '.app')
+current_app=$(/opt/homebrew/bin/yabai -m query --windows --window | /opt/homebrew/bin/jq -r '.app')
 
-# Get the list of all windows for the current application, then find the next window ID
-next_window=$(yabai -m query --windows | jq --arg app "$current_app" --argjson current_window "$current_window" '[.[] | select(.app == $app) | .id] | sort | [.[], .[0]] | unique | .[index($current_window)+1]')
+# Get a sorted list of all window IDs for the current application
+window_ids=$(/opt/homebrew/bin/yabai -m query --windows | /opt/homebrew/bin/jq --arg app "$current_app" '[.[] | select(.app == $app) | .id] | sort')
+
+# Get the total number of windows
+total_windows=$(echo $window_ids | /opt/homebrew/bin/jq 'length')
+
+# Get the index of the current window in the sorted list
+current_index=$(echo $window_ids | /opt/homebrew/bin/jq --argjson current_window "$current_window" 'index($current_window)')
+
+# Calculate the index of the next window (with wrapping)
+next_index=$(((current_index + 1) % total_windows))
+
+# Get the ID of the next window to focus
+next_window=$(echo $window_ids | /opt/homebrew/bin/jq ".[$next_index]")
 
 # Focus the next window
 if [ "$next_window" != "null" ]; then
-    yabai -m window --focus $next_window
+    /opt/homebrew/bin/yabai -m window --focus $next_window
 fi

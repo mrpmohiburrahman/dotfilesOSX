00dae35393de403205b447535eefb2b233ca8d2d
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-04-15 08:12:15 +0600

    leader a to select the whole file

--- nvim/VSCodeNeovim/init.lua
+++ nvim/VSCodeNeovim/init.lua
@@ -1,7 +1,21 @@
+-- <leader> key
+vim.g.mapleader = ' '
+
+
+-- Select all
+vim.api.nvim_set_keymap('n', '<leader>a', 'ggVG', { noremap = true, silent = true })
+
+-- keymap.set("n", "<leader>a", "gg<S-v>G")
+
 
 -- skip folds (down, up)
 vim.cmd('nmap j gj')
 vim.cmd('nmap k gk')
+
+-- -- Skip folds (down, up) using 'j' and 'k'
+-- vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+-- Added { noremap = true, silent = true } to the key mappings to ensure they do not allow rema
 
 -- sync system clipboard
 vim.opt.clipboard = 'unnamedplus'

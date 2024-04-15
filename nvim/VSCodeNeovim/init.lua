6e51e0e89486c458752a4e28b9534b5b2540f123
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-04-15 07:53:21 +0600

    sync clipboard and improve searches on vscode neovim

---
+++ nvim/VSCodeNeovim/init.lua
@@ -0,0 +1,8 @@
+-- sync system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- search ignoring case
+vim.opt.ignorecase = true
+
+-- disable "ignorecase" option if the search pattern contains upper case characters
+vim.opt.smartcase = true

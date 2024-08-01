31c5549eb2fe4a972fc8cf1b9efc491bd5c52533
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-08-01 07:28:10 +0600

    Prevent cursor jumping back to where selection started on yank

--- nvim/VSCodeNeovim/lunar.lua
+++ nvim/VSCodeNeovim/lunar.lua
@@ -3,7 +3,6 @@ local opts = {
     noremap = true,
     silent = true
 }
-
 -- <leader> key
 -- vim.g.mapleader = ' '
 
@@ -24,6 +23,8 @@ vim.cmd('nmap k gk')
 -- Added { noremap = true, silent = true } to the key mappings to ensure they do not allow rema
 
 -- Prevent cursor jumping back to where selection started on yank
+vim.cmd('vmap y ygv<Esc>')
+
 -- map("v", "y", "ygv<Esc>")
 -- sync system clipboard
 vim.opt.clipboard = 'unnamedplus'
@@ -36,52 +37,52 @@ vim.opt.ignorecase = true
 -- disable "ignorecase" option if the search pattern contains upper case characters
 vim.opt.smartcase = true
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
-end
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--     vim.fn.system(
+--         {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+--          lazypath})
+-- end
 
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({{
-    'vscode-neovim/vscode-multi-cursor.nvim',
-    event = 'VeryLazy',
-    cond = not not vim.g.vscode,
-    opts = {}
-    -- default mapping of muilti cursor nvim
-    -- local cursors = require('vscode-multi-cursor')
-    -- local k = vim.keymap.set
-    -- k({'n', 'x'}, 'mc', cursors.create_cursor, {
-    --     expr = true,
-    --     desc = 'Create cursor'
-    -- })
-    -- k({'n'}, 'mcc', cursors.cancel, {
-    --     desc = 'Cancel/Clear all cursors'
-    -- })
-    -- k({'n', 'x'}, 'mi', cursors.start_left, {
-    --     desc = 'Start cursors on the left'
-    -- })
-    -- k({'n', 'x'}, 'mI', cursors.start_left_edge, {
-    --     desc = 'Start cursors on the left edge'
-    -- })
-    -- k({'n', 'x'}, 'ma', cursors.start_right, {
-    --     desc = 'Start cursors on the right'
-    -- })
-    -- k({'n', 'x'}, 'mA', cursors.start_right, {
-    --     desc = 'Start cursors on the right'
-    -- })
-    -- k({'n'}, '[mc', cursors.prev_cursor, {
-    --     desc = 'Goto prev cursor'
-    -- })
-    -- k({'n'}, ']mc', cursors.next_cursor, {
-    --     desc = 'Goto next cursor'
-    -- })
-    -- k({'n'}, 'mcs', cursors.flash_char, {
-    --     desc = 'Create cursor using flash'
-    -- })
-    -- k({'n'}, 'mcw', cursors.flash_word, {
-    --     desc = 'Create selection using flash'
-    -- })
+-- vim.opt.rtp:prepend(lazypath)
+-- require("lazy").setup({{
+--     -- 'vscode-neovim/vscode-multi-cursor.nvim',
+--     -- event = 'VeryLazy',
+--     -- cond = not not vim.g.vscode,
+--     -- opts = {}
+--     -- default mapping of muilti cursor nvim
+--     -- local cursors = require('vscode-multi-cursor')
+--     -- local k = vim.keymap.set
+--     -- k({'n', 'x'}, 'mc', cursors.create_cursor, {
+--     --     expr = true,
+--     --     desc = 'Create cursor'
+--     -- })
+--     -- k({'n'}, 'mcc', cursors.cancel, {
+--     --     desc = 'Cancel/Clear all cursors'
+--     -- })
+--     -- k({'n', 'x'}, 'mi', cursors.start_left, {
+--     --     desc = 'Start cursors on the left'
+--     -- })
+--     -- k({'n', 'x'}, 'mI', cursors.start_left_edge, {
+--     --     desc = 'Start cursors on the left edge'
+--     -- })
+--     -- k({'n', 'x'}, 'ma', cursors.start_right, {
+--     --     desc = 'Start cursors on the right'
+--     -- })
+--     -- k({'n', 'x'}, 'mA', cursors.start_right, {
+--     --     desc = 'Start cursors on the right'
+--     -- })
+--     -- k({'n'}, '[mc', cursors.prev_cursor, {
+--     --     desc = 'Goto prev cursor'
+--     -- })
+--     -- k({'n'}, ']mc', cursors.next_cursor, {
+--     --     desc = 'Goto next cursor'
+--     -- })
+--     -- k({'n'}, 'mcs', cursors.flash_char, {
+--     --     desc = 'Create cursor using flash'
+--     -- })
+--     -- k({'n'}, 'mcw', cursors.flash_word, {
+--     --     desc = 'Create selection using flash'
+--     -- })
 
-}})
+-- }})

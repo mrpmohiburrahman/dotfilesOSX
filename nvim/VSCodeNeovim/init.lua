735d28145d1bfe5ef03c81ff2b24b3b79a26769e
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-11-20 21:22:41 +0600

    update

--- nvim/VSCodeNeovim/init.lua
+++ nvim/VSCodeNeovim/init.lua
@@ -1,31 +1,27 @@
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
-
+-- local keymap = vim.keymap
+-- local opts = {
+--     noremap = true,
+--     silent = true
+-- }
 -- <leader> key
-vim.g.mapleader = ' '
-
+-- vim.g.mapleader = ' '
 -- save
-vim.cmd('nmap <leader>w :w<cr>')
-
+-- vim.cmd('nmap <leader>w :w<cr>')
 -- Select all
-vim.api.nvim_set_keymap('n', '<leader>a', 'ggVG', opts)
-
+-- vim.api.nvim_set_keymap('n', '<leader>a', 'ggVG', opts)
 -- keymap.set("n", "<leader>a", "gg<S-v>G")
-
-
 -- skip folds (down, up)
-vim.cmd('nmap j gj')
-vim.cmd('nmap k gk')
+-- vim.cmd('nmap j gj')
+-- vim.cmd('nmap k gk')
 -- -- Skip folds (down, up) using 'j' and 'k'
 -- vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
 -- Added { noremap = true, silent = true } to the key mappings to ensure they do not allow rema
-
--- sync system clipboard
+-- sync system clipboard and don't remove the yanked item
 vim.opt.clipboard = 'unnamedplus'
 
--- search ignoring case
-vim.opt.ignorecase = true
+-- -- search ignoring case
+-- vim.opt.ignorecase = true
 
--- disable "ignorecase" option if the search pattern contains upper case characters
-vim.opt.smartcase = true
+-- -- disable "ignorecase" option if the search pattern contains upper case characters
+-- vim.opt.smartcase = true

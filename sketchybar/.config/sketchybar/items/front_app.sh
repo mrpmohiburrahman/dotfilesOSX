4f95820f0416ee87d1788b4fdc198c35bf5941f3
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2025-04-27 08:52:43 +0600

    ✨ working aerospace using lua from wolveix, Robert Thomas

--- sketchybar/.config/sketchybar/items/front_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item front_app left \
-           --set front_app       icon.color="$ACCENT_COLOR" \
-                                 icon.font="sketchybar-app-font:Regular:16.0" \
-                                 label.color="$ACCENT_COLOR" \
-                                 script="$PLUGIN_DIR/front_app.sh"            \
-                                 display="active" \
-           --subscribe front_app front_app_switched

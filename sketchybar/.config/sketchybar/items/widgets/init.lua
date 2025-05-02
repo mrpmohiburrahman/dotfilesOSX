15f7dcae3085219505f988ef6f9db5271a3b6b56
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2025-05-02 15:09:02 +0600

    ✨ add ram, ssd, weather

--- sketchybar/.config/sketchybar/items/widgets/init.lua
+++ sketchybar/.config/sketchybar/items/widgets/init.lua
@@ -3,3 +3,7 @@ require("items.widgets.battery")
 require("items.widgets.volume")
 require("items.widgets.cpu")
 require("items.widgets.wifi")
+require("items.widgets.ssd")
+require("items.widgets.ram")
+require("items.widgets.weather")
+

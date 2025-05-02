6db5cda961b2cf9e06ef94e063396c96c412f9d5
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2025-05-02 15:26:57 +0600

    💄 fix date and time

--- sketchybar/.config/sketchybar/items/calendar.lua
+++ sketchybar/.config/sketchybar/items/calendar.lua
@@ -1,33 +1,49 @@
 -- .config/sketchybar/items/calendar.lua
 local settings = require("settings")
+local colors = require("colors")
 
 local cal = sbar.add("item", {
     icon = {
+        color = colors.dirty_white,
         font = {
-            family = settings.font.text,
-            style = settings.font.style_map["Regular"],
-            size = settings.font.size
+            style = settings.font.style_map["Bold"],
+            size = 14.0
         },
-        padding_left = 8
+        y_offset = -1,
+        padding_right = -2
     },
     label = {
-        align = "right",
+        color = colors.dirty_white,
+        width = 120,
+        align = "left",
         font = {
-            family = settings.font.text,
-            style = settings.font.style_map["Regular"],
-            size = settings.font.size
-        },
-        padding_right = 10
+            style = settings.font.style_map["Bold"],
+            size = 14.0
+        }
     },
     position = "right",
-    update_freq = 30,
-    padding_left = 1,
-    padding_right = 1
+    update_freq = 1,
+    y_offset = 1,
+    padding_left = -2
 })
 
+-- german Date
 cal:subscribe({"forced", "routine", "system_woke"}, function(env)
+    local weekdayNames = {"Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."}
+    local monthNames = {"Jan.", "Feb.", "Mar", "Apr.", "May", "Jun", "July", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."}
+
     cal:set({
-        icon = os.date("%a %d %b"),
-        label = os.date("%I:%M %p")
+        icon = weekdayNames[tonumber(os.date("%w")) + 1] .. os.date("%d") .. " " ..
+            monthNames[tonumber(os.date("%m"))],
+        label = "｜" .. os.date("%I:%M:%S %p")
     })
 end)
+
+cal:subscribe("mouse.clicked", function(env)
+    sbar.exec("open -a 'Dato'")
+end)
+
+-- english date
+-- cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
+--   cal:set({ icon = os.date("%a. %d %b."), label = os.date("%H:%M") })
+-- end)
